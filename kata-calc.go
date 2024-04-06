6cf518562b07746d38792d89b011b7fd5e05181c
DenisKanafeev <dkanafeev@mail.ru>
2024-04-06 15:43:03 +0300

    Added comments, beautified code by using text constants

--- kata-calc.go
+++ kata-calc.go
@@ -16,37 +16,43 @@ func main() {
 	var input, x, y, op string
 	var xrom, yrom bool
 	var xint, yint, result int
+	const welcome = "This is a simple calculator for one-digit numbers. It understands romans and integers, but not at the same time."
+	const bye = "OK, bye then!"
+	const invop = "invalid operation"
+	const nosnum = "no second number provided"
+	const nofnum = "no first number provided"
+	const numrangeerr = "only numbers from 0 to 10 allowed"
+	const numsyserr = "numbers must be of a same system"
+	const romlessthanzero = "roman result < 0"
 
-	fmt.Println("This is a simple calculator for one-digit numbers.")
-	fmt.Println("It understands romans and integers, but not at the same time.")
+	fmt.Println(welcome)
 
 	for {
-		xrom, yrom = false, false
-		fmt.Println("Enter number operation number. To stop enter stop.")
+		xrom, yrom = false, false                                         //в начале цикла сбрасываем значения xrom и yrom на false
+		fmt.Println("Enter number operation number. To stop enter stop.") //запрашиваем у юзера строку
 		scanner := bufio.NewScanner(os.Stdin)
 		_ = scanner.Scan()
 		input = scanner.Text()
 
-		if input == "stop" {
-			fmt.Println("OK, bye then!")
+		if input == "stop" { //если юзер ввёл строку "stop", завершаем работу
+			fmt.Println(bye) //и прощаемся
 			break
 		}
 
-		input = strings.ReplaceAll(input, " ", "")
-		fmt.Println(input)
+		input = strings.ReplaceAll(input, " ", "") //удаляем все пробелы из строки
 
 		count := 0
-		for i := 0; i <= len(input)-1; i++ {
-			if input[i] == 42 || input[i] == 43 || input[i] == 45 || input[i] == 47 {
+		for i := 0; i <= len(input)-1; i++ { //при помощи счётчика проверяем, сколько арифметичеких операций указано в строке
+			if input[i] == 42 || input[i] == 43 || input[i] == 45 || input[i] == 47 { //символы операций смотрим в ASCII
 				count++
 			}
 		}
 
-		if count != 1 {
-			panic("Invalid operation")
+		if count != 1 { //если количество операций не равно 1, завершаем работу
+			panic(invop) //выводим сообщение об ошибке
 		}
 
-		switch {
+		switch { //проверяем, какую именно операцию указал юзер, и записываем её в переменную op
 		case strings.Contains(input, "+"):
 			op = "+"
 		case strings.Contains(input, "-"):
@@ -57,39 +63,39 @@ func main() {
 			op = "/"
 		}
 
-		x = strings.Split(input, op)[0]
-		y = strings.Split(input, op)[1]
+		x = strings.Split(input, op)[0] //делим строку по оператору и часть строки до операции записываем в переменную x
+		y = strings.Split(input, op)[1] //делим строку по оператору и часть строки после операции записываем в переменную y
 
-		if x == "" {
-			panic("no first number provided")
+		if x == "" { //если до операции ничего не указано, завершаем работу
+			panic(nofnum) //и выводим сообщение об ошибке (не указано первое число)
 		}
-		if y == "" {
-			panic("no second number provided")
+		if y == "" { //если после операции ничего не указано, завершаем работу
+			panic(nosnum) //и выводим сообщение об ошибке (не указано второе число)
 		}
 
-		if x[0] < 48 || x[0] > 57 {
-			xrom = true
-			xint = romtoint(x)
+		if x[0] < 48 || x[0] > 57 { //если первый символ первого числа не является цифрой (проверяем по ASCII)
+			xrom = true        //значит юзер указал первое число в римской системе, отражаем это в переменной xrom
+			xint = romtoint(x) //переводим его в int с помощью переводчика и записываем в переменную xint
 		} else {
-			xint, _ = strconv.Atoi(x)
+			xint, _ = strconv.Atoi(x) //если же первый сомвол это цифра, переводим строку в число и записываем в переменную xint
 		}
 
-		if y[0] < 48 || y[0] > 57 {
+		if y[0] < 48 || y[0] > 57 { //то же самое делаем со вторым числом
 			yrom = true
 			yint = romtoint(y)
 		} else {
 			yint, _ = strconv.Atoi(y)
 		}
 
-		if xint < 1 || xint > 10 || yint < 1 || yint > 10 {
-			panic("only numbers from 0 to 10 allowed")
+		if xint < 1 || xint > 10 || yint < 1 || yint > 10 { //если одно из чисел меньше 1 или больше 10
+			panic(numrangeerr) //завершаем работу и показываем сообщение об ошибке
 		}
 
-		if xrom != yrom {
-			panic("numbers must be of a same system")
+		if xrom != yrom { //если числа указаны в разных системах
+			panic(numsyserr) //завершаем работу и показываем сообщение об ошибке
 		}
 
-		switch {
+		switch { //вычисляем результат операции с числами и записываем его в переменную result
 		case op == "+":
 			result = xint + yint
 		case op == "-":
@@ -98,51 +104,49 @@ func main() {
 			result = xint * yint
 		case op == "/":
 			result = xint / yint
-		default:
-			panic("only +, -, * or / allowed")
 		}
 
-		if xrom && result < 0 {
-			panic("roman result < 0")
+		if xrom && result < 0 { //если юзер указывал римские цифры, а результат получился меньше 0
+			panic(romlessthanzero) //завершаем работу и показываем сообщение об ошибке
 		}
 
-		if xrom == yrom && xrom {
-			fmt.Println(x, op, y, "=", inttorom(result))
-		} else if xrom == yrom && !xrom {
-			fmt.Println(x, op, y, "=", result)
+		if xrom { //если юзер вводил римские цифры
+			fmt.Println(x, op, y, "=", inttorom(result)) //выводим результат в римской системе с помощью обратного переводчика
+		} else {
+			fmt.Println(x, op, y, "=", result) //если же нет, то просто выводим результат
 		}
 	}
 }
 
-func inttorom(num int) string {
-	rom := ""
-	if num == 100 {
+func inttorom(num int) string { //это функция-переводчик для перевода int чисел до 100 (int) в римские (string)
+	rom := ""       //сейчас будем считать десятки в числе и записывать их в переменную rom
+	if num == 100 { //100 это исключение, поэтому переводим его буквально
 		rom = "C"
-	} else if num >= 90 {
+	} else if num >= 90 { //если число больше 90, записываем в rom значение "XC", а оставшиеся единицы сохраняем в num
 		rom = "XC"
 		num = num - 90
-	} else if num >= 50 {
+	} else if num >= 50 { //если число больше 50, записываем в rom значение "XC", а оставшиеся единицы сохраняем в num
 		rom = "L"
 		num -= 50
 		for num > 10 {
 			rom += "X"
 			num -= 10
 		}
-	} else if num >= 40 {
+	} else if num >= 40 { //если число больше 40, записываем в rom значение "XC", а оставшиеся единицы сохраняем в num
 		rom = "XL"
 		num -= 40
-	} else if num >= 10 {
+	} else if num >= 10 { //если число больше 10
 		for num >= 10 {
-			rom += "X"
-			num -= 10
+			rom += "X" //добавляем в rom значение "X"
+			num -= 10  //пока десятки не закончатся
 		}
 	}
-	return rom + romless10(num)
+	return rom + inttoromless10(num) //добавляем к десяткам единицы с помощью переводчика единиц и возвращаем
 }
 
-func romless10(num int) string {
+func inttoromless10(num int) string { //это функция-переводчик для перевода int цифр (int) в римские цифры (string)
 	rom := ""
-	if num == 9 {
+	if num == 9 { //она буквально берёт каждую цифру и переводит римскую строку
 		rom = "IX"
 	} else if num == 8 {
 		rom = "VIII"
@@ -161,18 +165,18 @@ func romless10(num int) string {
 	} else if num == 1 {
 		rom = "I"
 	}
-	return rom
+	return rom //возвращаем количество единиц в римской системе
 }
 
-func romtoint(rom string) int {
+func romtoint(rom string) int { //это функция-переводчик цифр из римских (string) в обычные (int)
 	num := 0
-	if rom == "IV" || rom == "iv" || rom == "iV" || rom == "Iv" {
-		num = 4
-	} else if rom == "IX" || rom == "ix" || rom == "iX" || rom == "Ix" {
-		num = 9
+	if rom == "IV" || rom == "iv" || rom == "iV" || rom == "Iv" { //цифры 4 и 9 в римской системе являются исключениями
+		num = 4 //потому что для их получения требуется вычитание
+	} else if rom == "IX" || rom == "ix" || rom == "iX" || rom == "Ix" { //поэтому переводим их буквально
+		num = 9 //при этом допускаем, что юзер ввёл их разными сочетаниями больших и маленьких букв
 	} else {
-		for i := 0; i <= len(rom)-1; i++ {
-			if rom[i] == 73 || rom[i] == 105 {
+		for i := 0; i <= len(rom)-1; i++ { //для остальных цифр используем цикл, где они складываются
+			if rom[i] == 73 || rom[i] == 105 { //опять же позволяя юзеру использовать разные сочетания больших и маленьких букв
 				num += 1
 			} else if rom[i] == 86 || rom[i] == 118 {
 				num += 5
@@ -181,5 +185,5 @@ func romtoint(rom string) int {
 			}
 		}
 	}
-	return num
+	return num //возвращаем получившуюся цифру в int формате
 }
